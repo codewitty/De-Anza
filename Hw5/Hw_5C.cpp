28d41151a1b730736852ef2ffc47b3a2c184056b
rachel1105 <gomesjoshua@gmail.com>
2018-11-01 11:24:31 -0700

    working model of C. Only need output file

--- Hw5/Hw_5C.cpp
+++ Hw5/Hw_5C.cpp
@@ -11,17 +11,54 @@
  message, explaining what is the purpose of the program, and an end of the program another message that includes 
  the name of the output file.
  
- NAME: 
+ NAME: JOSHUA N. GOMES 
  */
-#include<iostream>
-#include<fstream>
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <cstdlib>
+
 using namespace std;
 
 int main()
 {
-    
-    
-   
+	const int MAX = 30;
+	const int MIN = 10;
+	int rand_num;
+	int ctr;
+	string name;
+	int guess;
+	char playerChoice;
+	
+
+	do
+	{
+		cout << "This is a fun guessing game that allows multiple players and also saves their results in a file called players.txt\n"
+	   	     << "Please enter your name\n";
+		cin >> name;
+		
+		do
+		{
+			rand_num = rand() % (MAX - MIN + 1) + MIN;
+			for (ctr = 1; ctr <= 5 ; ctr++)
+				{
+					cout << "Please guess a number between 10 and 30\n";
+					cin >> guess;
+					if (rand_num == guess)
+						cout << "Congratulations " << name << ", you have guessed the correct number in " << ctr << " tries!\n";
+					else if (guess < 10 || guess > 30)
+						cout << "Please enter a valid choice between 10 and 30\n";
+					else
+						cout << "Try again\n";
+				}
+			if (rand_num != guess)
+				cout << "Sorry " << name << ", the random number was " << rand_num << "!\n";	
+			cout << "Would you like to play again?\nType Y or y for Yes and anything else for no.\n";
+			cin >> playerChoice;
+		}while (playerChoice == 'Y' || playerChoice == 'y');
+   		cout << "Is there another player who would like to play?\nType Y or y for Yes and anything else for no.\n";
+		cin >> playerChoice;
+	}while (playerChoice == 'Y' || playerChoice == 'y');
     return 0;
 }
 /**~*~*~*~*~*~*~*~*~*
