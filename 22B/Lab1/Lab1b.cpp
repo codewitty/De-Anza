7dca2897199b16fdfbefb673f99085fd2e66c9b0
codewitty <35624908+codewitty@users.noreply.github.com>
2019-01-11 16:11:27 -0800

    Lab1 final

--- 22B/Lab1/Lab1b.cpp
+++ 22B/Lab1/Lab1b.cpp
@@ -4,9 +4,9 @@
 // 		to the screen.
 //
 // What's on your mind about this lab? 
-// This exercise helps me to continut to understand 
-// the basic concepts of using 2D arrays, how to initialise them, 
-// pass them to functions and print them to the screen.
+// This exercise helps me to understand how pointers are used
+// how to initialise them, pass them to functions 
+// and use loops to print them to the screen.
 //
 // Author: Joshua Nelson Gomes 
 //
@@ -23,28 +23,39 @@ using namespace std;
 
 
 int main() {
+	//define int constant
 	const int NUM_EXPENSES = 7;
+	//define and initialise variables, pointer and array
 	double sum = 0;
 	double average;
 	double expenses[NUM_EXPENSES] = {
 		45.25, 49.38, 12.75, 123.50, 8.25, 19.95,
 		145.70};
-	double *ptr = nullptr;
+	double *ptr = expenses;
 
-	for (int i = 0; i < NUM_EXPENSES; i++) {
-		sum += expenses[i];
+	/*iterate through the array using ptr to calculate the total 
+	 *of the expenses.*/
+	for (ptr; ptr < &expenses[NUM_EXPENSES]; ptr++) {
+		sum += *ptr;
 	}
 
+	//use the total to find the average of the expenses
 	average = sum / NUM_EXPENSES;
+	cout << sum << endl << average << endl << endl; 
+	ptr = expenses;
+	/* This block of code prints the report header first. It then uses 
+	 * the pointer variable to iterate through the expenses array to 
+	 * print the expenses. Finally it prints the total and the average.
+	*/
 
-
-	//print report
+	//print header
 	cout << " #              Amount" << endl;
     	cout << "----------------------" << endl;
-
+	
+	//loop through array and print it along with total and average
 	for (int i = 1; i <= NUM_EXPENSES; i++) {
-		cout	<< " " << i << setw(20) << right << fixed << setprecision(2)
-			<< expenses[i - 1] << endl;
+		cout	<< " " << i << setw(20) << right << fixed 
+			<< setprecision(2) << ptr[i - 1] << endl;
 	}
 
 	cout	<< "Total" << setw(17) << right
