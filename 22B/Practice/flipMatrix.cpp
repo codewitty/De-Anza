cec984bd078ab3910dd3da7bd2f08a6b67282ea4
Joshua Gomes <gomesjoshua@gmail.com>
2019-03-10 13:17:55 -0700

    new fns

---
+++ 22B/Practice/flipMatrix.cpp
@@ -0,0 +1,5 @@
+/*
+1. flipMatrix that takes a matrix (2-dim array) and flips it around the middle vertical line that separates the left from the right.
+2. printMatirx that takes a matrix (2-dim array) and prints its contents.
+Test the flip function using a matrix defined in the main function. First print the matrix, then flip it and print the result, then flip it again and print the result.
+*/
