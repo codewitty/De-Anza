9f9d3a0b7fad6962dd29b0e2c0bfde6a8037c0ce
Joshua Gomes <gomesjoshua@gmail.com>
2019-03-10 12:53:32 -0700

    new fns

---
+++ 22B/Practice/Strings.cpp
@@ -0,0 +1,5 @@
+/*
+Write a function named shorten that receives a string object and an integer. The function returns a string formed from the first n characters concatenated with the last n characters of the string parameter where n is the integer parameter, n is less than or equal to one half of the length of the string. If the result length is shorter, insert a "..." between the parts. For example, if the string is "CIS 22B Intermediate Programming Methodology in C++", and the first 10 and the last 10 are wanted, the result should be "CIS 22B In...logy in C++".
+Test the function using 3 test strings.
+*/
+
