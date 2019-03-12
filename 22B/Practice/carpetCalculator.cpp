6ff819175843bb13f8dd5fc83acf0a534717e3a2
Joshua Gomes <gomesjoshua@gmail.com>
2019-03-12 16:41:31 -0700

    new fns

---
+++ 22B/Practice/carpetCalculator.cpp
@@ -0,0 +1,3 @@
+/*
+Chapter 14, Program Challenges #13. Write this program with classes of your own design except the FeetInches class demonstrated in the chapter. Do not look at existing solutions.
+*/
