bb4a3d8448db9542ddfd8d958bc5a7fd99069fc4
Joshua Gomes <gomesjoshua@gmail.com>
2019-03-11 23:18:47 -0700

    new fns

---
+++ 22B/Practice/advancedFileOps.cpp
@@ -0,0 +1,18 @@
+/*
+Last Line 
+
+Write a program that displays the last line of the file. If the file doesn't exist, display a message that the file cannot be opened. If the line is empty, display a message that the file is empty.
+
+Each line in the file ends with '\n'.
+
+Tail Program
+
+Write a program that asks the user for the name of a file and a number. The program should display the tail portion of the file using the number. For example, if the number is 10, the program should display last 10 lines of the file on the screen. If the file has fewer lines than the provided number, the entire file should be displayed, with a message indicating the entire file has been displayed. If the number is negative, display a message that the number cannot be negative. If the file doesn't exist, display a message that the file cannot be opened.
+
+Hint: Use seekg and the like member functions. Each line in the file ends with '\n'.
+
+Array/File Functions
+
+Chapter 12 Advanced File Operations Programming Challenges #8
+*/
+
