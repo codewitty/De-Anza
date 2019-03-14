67dcc6a7f48ea823386969469f5faa143bd88678
Joshua Gomes <gomesjoshua@gmail.com>
2019-03-14 10:15:17 -0700

    new fns

--- 22B/Practice/aGameof21.cpp
+++ 22B/Practice/aGameof21.cpp
@@ -1,5 +1,6 @@
 /*
-Chapter 13, Program Challenges #18. Write this program with classes of your own design except the Die class demonstrated in the chapter. Do not look at existing solutions.
+Chapter 13, Program Challenges #18. Write this program with classes of your own design except 
+the Die class demonstrated in the chapter. Do not look at existing solutions.
 
 Suggestion: write at least two classes named Player and Dealer.
 */
