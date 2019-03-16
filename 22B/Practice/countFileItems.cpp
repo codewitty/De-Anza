f08f4d102d7bb1707f22b21f08c141c50cbfa471
Joshua Gomes <gomesjoshua@gmail.com>
2019-03-16 14:02:50 -0700

    this

--- 22B/Practice/countFileItems.cpp
+++ 22B/Practice/countFileItems.cpp
@@ -1,7 +1,8 @@
 /*
 Count Items
 
-Write a function that counts the integers existing in a file and returns the count. The integers are separated by white spaces. If the file doesn't exist, display a message that the file cannot be opened. 
+Write a function that counts the integers existing in a file and returns the count. 
+The integers are separated by white spaces. If the file doesn't exist, display a message that the file cannot be opened. 
 
 Test the function using a call with parameter(s).
 */
