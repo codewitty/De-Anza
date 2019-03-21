640ad0d946965183c09bef97dbefa4047a49e6ec
Joshua Gomes <gomesjoshua@gmail.com>
2019-03-20 21:54:18 -0700

    changes

--- 22B/Practice/advancedFileOps.cpp
+++ 22B/Practice/advancedFileOps.cpp
@@ -13,7 +13,8 @@ Tail Program
 Write a program that asks the user for the name of a file and a number. 
 The program should display the tail portion of the file using the number. 
 For example, if the number is 10, the program should display last 10 lines of the file on the screen. 
-If the file has fewer lines than the provided number, the entire file should be displayed, with a message indicating the entire file has been displayed. If the number is negative, display a message that the number cannot be negative. If the file doesn't exist, display a message that the file cannot be opened.
+If the file has fewer lines than the provided number, the entire file should be displayed, 
+with a message indicating the entire file has been displayed. If the number is negative, display a message that the number cannot be negative. If the file doesn't exist, display a message that the file cannot be opened.
 
 Hint: Use seekg and the like member functions. Each line in the file ends with '\n'.
 
