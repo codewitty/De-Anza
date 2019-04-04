f19abf92d74098c76b3cbd78e3bef6a4d32213a7
Joshua Gomes <gomesjoshua@gmail.com>
2019-04-03 22:35:36 -0700

    chnages

--- 22B/Practice/CharStrings.cpp
+++ 22B/Practice/CharStrings.cpp
@@ -1,5 +1,6 @@
 /*
-Write a function named maxOccuringChar that can find the maximum occurring character in a C-string.
+Write a function named maxOccuringChar that can find the 
+maximum occurring character in a C-string.
 Test the function using 3 test C-strings of lengths 11, 13 and 31
 */
 
