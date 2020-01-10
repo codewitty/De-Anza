698fef5bf3139a7b9e33f674707738b085e390d7
Joshua Gomes <gomesjoshua@gmail.com>
2020-01-09 18:30:08 -0800

    added implementation

---
+++ 22C/EC1/counter.cpp
@@ -0,0 +1,35 @@
+#include "Counter.h"
+
+
+
+int counter;
+int limit;
+
+int Counter::nCounters=0;
+
+Counter::Counter(int a, int b)
+{
+    counter = a;
+    limit = b;
+    nCounters++;
+}
+
+void Counter::increment(){
+    if (counter < limit) {
+        counter++;
+    }
+}
+
+void Counter::decrement(){
+    if(counter > 0){
+        counter--;
+    }
+}
+
+int Counter::getValue(){
+    return counter;
+}
+
+int Counter::getNCounters(){
+    return nCounters;
+}
