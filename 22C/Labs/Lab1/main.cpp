36b09a705bb51cdb49dde55651820ebf3f168f39
Joshua Gomes <gomesjoshua@gmail.com>
2020-01-14 02:09:33 -0800

    changes to main, added switch case for initial choice

--- 22C/Labs/Lab1/main.cpp
+++ 22C/Labs/Lab1/main.cpp
@@ -5,6 +5,48 @@ using namespace std;
 int main() {
 
 	Wallet myWallet;
-	cout << "Which currency would you like to select?" << endl
-		<< "Type 1 for Dollars"
+	
+	int select == 0;
+	
+	while (select == 0){
+		int choice;
+			
+		cout 	<< "What would you like to do with your wallet today?"
+			<< endl
+			<< "1: Add Money" << endl;
+       			<< "2: Remove Money" << endl;
+       			<< "3: View Wallet" << endl;
+       			<< "4: Empty Wallet" << endl;
+       			<< "5: EXIT" << endl << endl;
+       			<< "Enter your Selection" << endl;
+		cin << choice;
+
+		switch (choice) {
+			case 1:
+				addMoney(myWallet);
+				break;
+			case 2:
+           			removeMoney(myWallet);
+           			break;
+
+       			case 3:
+           			viewWallet(myWallet);
+           			break;
+
+       			case 4:
+           			emptyWallet(myWallet);
+           			break;
+
+       			case 5:
+           			loop = true;
+           			break;
+
+       			default:
+           			cout << "Please enter a valid choice from 1-5" << endl; 
+           			break;
+		}
+
+		return 0;
+}
+
 
