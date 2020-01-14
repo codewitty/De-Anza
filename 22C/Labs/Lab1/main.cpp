e12e8a443ece307a90781204a339a52b90d29543
Joshua Gomes <gomesjoshua@gmail.com>
2020-01-14 00:00:55 -0800

    new lab

--- 22C/Labs/Lab1/main.cpp
+++ 22C/Labs/Lab1/main.cpp
@@ -4,6 +4,7 @@ using namespace std;
 
 int main() {
 
-	Wallet myWallet[5];
-	cout << "Which currency would you like to select?" << endl;
+	Wallet myWallet;
+	cout << "Which currency would you like to select?" << endl
+		<< "Type 1 for Dollars"
 
