5233378b910ed23ee1c2a994fb7c8867074aee3d
Joshua Gomes <gomesjoshua@gmail.com>
2020-01-24 09:34:26 -0800

    new codelabs

--- 22C/CodeLabs/10676.cpp
+++ 22C/CodeLabs/10676.cpp
@@ -12,34 +12,18 @@ The function can be used as follows:
 	small is now 5 
 		*/
 
-void minMax(int a,int b,int c,int &big,int &small)
+void minMax(int a,int b,int c,int *big,int &small)
 {
     int max;
     int min;
-   /* 
+
     max = (a > b) ? a : b;
     max = (max > c) ? max : c;
     min = (a < b) ? a : b;
     min = (min < c) ? min : c;
-   */
 
-   if (a > b) 
-   {
-	   max = a;
-	   min = b;
-   }
-   else
-   {
-		max = b;
-		min = a;
-   }
-	if (max < c)
-		max = c;
-	else if (min > c)
-		min = c;
-	
-    big = max;
-    small = min;
+    *big = max;
+    *small = min;
 }
     
     
