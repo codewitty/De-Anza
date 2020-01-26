d2843685349e9a85755f7f67728aefc4b6e0f780
Joshua Gomes <gomesjoshua@gmail.com>
2020-01-25 21:17:20 -0800

    new codel

--- 22C/CodeLabs/10775.cpp
+++ 22C/CodeLabs/10775.cpp
@@ -1,4 +1,7 @@
+Write a recursive, int-valued function named productOfOdds that accepts an integer array, and the number of elements in the array and returns the product of the odd-valued elements of the array. You may assume the array has at least one odd-valued element. The product of the odd-valued elements of an integer-valued array recursively may be calculated as follows:
 
+If the array has a single element and it is odd, return the value of that element; otherwise return 1.
+Otherwise, if the first element of the array is odd, return the product of that element and the result of finding the product of the odd elements of the rest of the array; if the first element is NOT odd, simply return the result of finding the product of the odd elements of the rest of the array
 
 
 int productOfOdds(int arr[], int n)
